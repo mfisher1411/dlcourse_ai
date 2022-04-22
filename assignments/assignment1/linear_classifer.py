fde67cc0b6bb6db97e6b72d065fdf2baf711cb86
mfisher <matthewfisher1411@gmail.com>
2022-04-22 10:24:50 +0500

    Implementation LinearSoftmaxClassifier()

--- assignments/assignment1/linear_classifer.py
+++ assignments/assignment1/linear_classifer.py
@@ -150,66 +150,81 @@ def linear_softmax(X, W, target_index):
     
 
 
-# class LinearSoftmaxClassifier():
-#     def __init__(self):
-#         self.W = None
+class LinearSoftmaxClassifier():
+    def __init__(self):
+        self.W = None
 
-#     def fit(self, X, y, batch_size=100, learning_rate=1e-7, reg=1e-5,
-#             epochs=1):
-#         '''
-#         Trains linear classifier
+    def fit(self, X, y, batch_size=100, learning_rate=1e-7, reg=1e-5,
+            epochs=1):
+        '''
+        Trains linear classifier
         
-#         Arguments:
-#           X, np array (num_samples, num_features) - training data
-#           y, np array of int (num_samples) - labels
-#           batch_size, int - batch size to use
-#           learning_rate, float - learning rate for gradient descent
-#           reg, float - L2 regularization strength
-#           epochs, int - number of epochs
-#         '''
+        Arguments:
+          X, np array (num_samples, num_features) - training data
+          y, np array of int (num_samples) - labels
+          batch_size, int - batch size to use
+          learning_rate, float - learning rate for gradient descent
+          reg, float - L2 regularization strength
+          epochs, int - number of epochs
+        '''
 
-#         num_train = X.shape[0]
-#         num_features = X.shape[1]
-#         num_classes = np.max(y)+1
-#         if self.W is None:
-#             self.W = 0.001 * np.random.randn(num_features, num_classes)
+        num_train = X.shape[0]
+        num_features = X.shape[1]
+        num_classes = np.max(y)+1
+        if self.W is None:
+            self.W = 0.001 * np.random.randn(num_features, num_classes)
 
-#         loss_history = []
-#         for epoch in range(epochs):
-#             shuffled_indices = np.arange(num_train)
-#             np.random.shuffle(shuffled_indices)
-#             sections = np.arange(batch_size, num_train, batch_size)
-#             batches_indices = np.array_split(shuffled_indices, sections)
+        loss_history = []
+        for epoch in range(epochs):
+            shuffled_indices = np.arange(num_train)
+            np.random.shuffle(shuffled_indices)
+            sections = np.arange(batch_size, num_train, batch_size)
+            batches_indices = np.array_split(shuffled_indices, sections)
 
-#             # TODO implement generating batches from indices
-#             # Compute loss and gradients
-#             # Apply gradient to weights using learning rate
-#             # Don't forget to add both cross-entropy loss
-#             # and regularization!
-#             raise Exception("Not implemented!")
+            # TODO implement generating batches from indices
+            # Compute loss and gradients
+            # Apply gradient to weights using learning rate
+            # Don't forget to add both cross-entropy loss
+            # and regularization!
+            
+            for indices in batches_indices:
+                X_batch = X[indices]
+                y_batch = y[indices]
+                loss, grad = linear_softmax(X_batch, self.W, y_batch)
+                loss_reg, grad_reg = l2_regularization(self.W, reg)
+                
+                loss += loss_reg
+                grad += grad_reg
+                
+                self.W -= grad * learning_rate
+                
+                loss_history.append((epoch,loss))
+            print("Epoch %i, loss: %f" % (epoch, loss))
+        return loss_history
+        raise Exception("Not implemented!")
 
-#             # end
-#             print("Epoch %i, loss: %f" % (epoch, loss))
+            # end
+            #print("Epoch %i, loss: %f" % (epoch, loss))
 
-#         return loss_history
+        #return loss_history
 
-#     def predict(self, X):
-#         '''
-#         Produces classifier predictions on the set
+    def predict(self, X):
+        '''
+        Produces classifier predictions on the set
        
-#         Arguments:
-#           X, np array (test_samples, num_features)
+        Arguments:
+          X, np array (test_samples, num_features)
 
-#         Returns:
-#           y_pred, np.array of int (test_samples)
-#         '''
-#         y_pred = np.zeros(X.shape[0], dtype=np.int)
+        Returns:
+          y_pred, np.array of int (test_samples)
+        '''
+        y_pred = np.zeros(X.shape[0], dtype=np.int)
 
-#         # TODO Implement class prediction
-#         # Your final implementation shouldn't have any loops
-#         raise Exception("Not implemented!")
+        # TODO Implement class prediction
+        # Your final implementation shouldn't have any loops
+        raise Exception("Not implemented!")
 
-#         return y_pred
+        return y_pred
 
 
 
